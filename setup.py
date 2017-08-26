9ed6cda8585cba7f5917284416abb829376d8085
David Lewis <david@dglc.com>
2017-08-26 12:05:06 -0400

    tweaked signalk example

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'DESC.rst'), encoding='utf-8') as f:
 
 setup (
   name='pivac',
-  version='0.7.4',
+  version='0.7.5',
   description='Raspberry Pi input utilities',
   long_description=long_description,
 
