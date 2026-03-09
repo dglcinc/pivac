5468b40faf345a7e374fe2b5564467d38bb8653b
David Lewis <david@dglc.com>
2026-03-09 17:01:01 -0400

    changed pivac to use venv

--- scripts/pivac-provider.sh
+++ scripts/pivac-provider.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-COMMAND="python $DIR/pivac-provider.py $@"
+COMMAND="/home/pi/pivac-venv/bin/python3 $DIR/pivac-provider.py $@"
 
 exec $DIR/provider.sh $COMMAND &
