7fd10b90b77b889153c75fb194ecec2a9e00a6eb
David Lewis <david@dglc.com>
2026-03-09 18:08:13 -0400

    Added Arduino modules

---
+++ pivac/ArduinoThermPSI.py
@@ -0,0 +1,50 @@
+import requests
+import logging
+import re
+import ast
+
+logger = logging.getLogger(__name__)
+
+def status(config = {}, output = "default"):
+    result = {}
+    sensors = {}
+
+    if "ipaddr" not in config:
+        logger.exception("No IP address specified in config file.")
+        raise ValueError
+    if "inputs" not in config:
+        logger.exception("No inputs specified in config file.")
+        raise ValueError
+    else:
+        sensors = config["inputs"]
+
+    if output == "signalk":
+        logger.debug("prepping sk output...")
+        from pivac import sk_init_deltas, sk_add_source, sk_add_value
+        deltas = sk_init_deltas()
+        sk_source = sk_add_source(deltas)
+
+    try:
+        logger.debug("Parsing pressure response...")
+        r = requests.get("http://%s" % config["ipaddr"], timeout=2)
+        logger.debug("Got request: %s" % r.text)
+        psi = ast.literal_eval(re.findall('.*\\{.*\}',r.text)[0])['psi']
+
+        if output == "signalk":
+            sk_add_value(sk_source,"%s.%s" % (sensors["psi"]["sk_path"], sensors["psi"]["outname"]), psi)
+        else:
+            result["outname"] = psi
+    except:
+        logger.exception("Exception collecting data from Arduino")
+
+    if output == "signalk":
+        logger.debug("deltas = %s" % deltas)
+        return deltas
+    else:
+        logger.debug("result = %s" % result)
+        return result
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(name)s %(levelname)s:%(asctime)s %(message)s',datefmt='%m/%d/%Y %I:%M:%S',level="DEBUG")
+
+    status()
